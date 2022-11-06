7b55d3320a948155a989fe132540ac6268380783
BIGOSCAR85 <rojas.osc85@gmail.com>
2022-11-06 21:46:46 +0000

    ultima modificacion

--- src/app.js
+++ src/app.js
@@ -6,30 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 //write your code here
-window.onload = function() {
-  let domains = getAllCombinations(pronoun, adj, noun, extension);
-  let output = document.getElementById("dominios");
-  output.innerHTML = domains.join("<br>");
+window.onload = () => {
+  document.getElementById("btn").addEventListener("click", setRandomName);
 };
 
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-let extension = [".com", ".es", ".io", ".us"];
+const pronoun = ["the", "our"];
+const adj = ["great", "big"];
+const noun = ["jogger", "racoon"];
+const extension = [".com", ".es", ".io", ".us"];
 
-function getAllCombinations(list1, list2, list3, list4) {
-  let domain = "";
-  let domains = [];
-  list1.forEach(l1 => {
-    list2.forEach(l2 => {
-      list3.forEach(l3 => {
-        list4.forEach(l4 => {
-          domain = `${l1}${l2}${l3}${l4}`;
-          domains.push(domain);
-          console.log(domain);
-        });
-      });
-    });
-  });
-  return domains;
-}
+const getRandomNumber = max => Math.floor(Math.random() * max);
+
+const getRandomName = () =>
+  `${pronoun[getRandomNumber(pronoun.length)]} ${
+    adj[getRandomNumber(adj.length)]
+  } ${noun[getRandomNumber(noun.length)]} ${
+    extension[getRandomNumber(extension.length)]
+  }`;
+
+const setRandomName = () => {
+  document.getElementById("dominios").innerHTML = getRandomName();
+};
+
+//document.getElementById("btn").addEventListener("click", setRandomName);
+//setRandomName();
